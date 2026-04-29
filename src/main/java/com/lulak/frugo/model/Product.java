3c6181b0580b223ae440239d486aa284698e1897
DominikLulak <duskthewoof@gmail.com>
2026-04-29 20:33:36 +0200

    migrate product storage from JSON to H2 database

--- src/main/java/com/lulak/frugo/model/Product.java
+++ src/main/java/com/lulak/frugo/model/Product.java
@@ -1,29 +1,35 @@
 package com.lulak.frugo.model;
 
-import javax.print.DocFlavor;
+import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
 public class Product {
-    private int id;
+
+    @Id
+    private Integer id;
+
     private String name;
     private String category;
     private String image;
+
+    @ElementCollection
     private List<String> variants;
 
-    public Product() {}
+    public Product(){}
 
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getCategory() {return category;}
-    public void setCategory(String category) {this.category = category;}
+    public String getCategory() { return category; }
+    public void  setCategory(String category) { this.category = category; }
 
-    public String getImage() {return image;}
-    public void setImage(String image) {this.image = image;}
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
 
-    public List<String> getVariants() {return variants;}
-    public void setVariants(List<String> variants) {this.variants = variants;}
+    public List<String> getVariants() { return variants; }
+    public void setVariants(List<String> variants) { this.variants = variants; }
 }
